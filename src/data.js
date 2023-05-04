f1e150989a8efd6309efb47b43ec15ad6920eba6
Bun-Boo <ios02052001@gmail.com>
2023-05-04 15:04:12 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://img.pikbest.com/png-images/qiantu/cartoon-fashion-shopping-beauty-pictures_2516414.png!w700wp",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://dony.vn/wp-content/uploads/2022/03/thue-nguoi-mau-chup-thoi-trang.jpg",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://media.vov.vn/sites/default/files/styles/large/public/2022-11/ton-33.jpg",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://leeandtee.vn/uploads/images/tin-tuc-tong-hop/mau-thoi-trang-hoan-thien-anh-vintage-1.jpg",
+    title: "WOMEN CLOTHES",
+    cat: "women",
+  },
+  {
+    id: 2,
+    img: "https://images2.thanhnien.vn/uploaded/thuyttl/2020_11_16/fashionman_hai/img_7753_NRVI.jpg?width=500",
+    title: "MEN CLOTHES",
+    cat: "men",
+  },
+  {
+    id: 3,
+    img: "https://lh4.googleusercontent.com/TE03Jj_53joR8L3AuEqydzvamMomSIcfa_SFtiRZhssVRP7xcXNXhjLtAYLBxidAo2EQ9POKLzP_F5KmNUTjWQZEroULeZcg9HmpCH0KZTJYyyVJNIwdU-mDhqN7pg1t6djzLsGh",
+    title: "BABY CLOTHES",
+    cat: "baby",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+];
