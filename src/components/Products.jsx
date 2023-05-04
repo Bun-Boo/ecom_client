b364f29290310758e9c12ce7882924c6126aa16d
Bun-Boo <ios02052001@gmail.com>
2023-05-04 15:15:53 +0700

    change link api

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -22,8 +22,8 @@ const Products = ({ cat, filter, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : `http://localhost:5000/api/products`
+            ? `https://api-hair-ecom.onrender.com/api/products?category=${cat}`
+            : `https://api-hair-ecom.onrender.com/api/products`
         );
         setProducts(res.data);
       } catch (error) {}
